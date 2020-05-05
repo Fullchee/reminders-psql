dfe7f6e2596bd194983646e5536f872f6c3d82d3
Fullchee Zhang <fullchee@gmail.com>
2020-05-04 22:14:52 -0400

    use the .graphql file instead of a string

--- src/schema.ts
+++ src/schema.ts
@@ -3,19 +3,15 @@ dotenv.config();
 const makeExecutableSchema = require("graphql-tools").makeExecutableSchema;
 
 const resolver = require("./resolver").resolver; // our resolver from resolver.js
+import fs from "fs";
+import path from "path";
 
 // define our user type
 // then define a users query, which must return an array type that optionally contains 0 or more User types
-const typeDefs = `
-  type User {
-    id: String!
-    name: String
-    email: String
-  }
-  type Query {
-    users: [User]!
-  }
-`;
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, "schema.graphql"),
+  "utf8"
+);
 
 export default makeExecutableSchema({
   typeDefs,
